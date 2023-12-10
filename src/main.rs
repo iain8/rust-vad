4ae462afe0e5f57948e8508a0069706da6d0fd3b
Iain Buchanan <ibuchanan@replicant.ai>
2023-12-10 10:26:40 -0500

    format the time

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array1, Array3, Axis};
+use ndarray::{Array3, Axis};
 use ort::{inputs, Error, Session};
 use wavers::{Samples, Wav};
 
@@ -9,16 +9,29 @@ fn load_model() -> Result<Session, Error> {
         .with_model_from_file("./models/silero_vad.onnx")
 }
 
-fn get_window_size() -> usize {
-    let sample_rate = 16000;
-    // let frame_size = 64;
+// returns a chunk of size 100ms
+fn get_window_size(sample_rate: i32) -> usize {
+    sample_rate as usize / 10
+}
 
-    sample_rate / 10
+fn format_time(ms: i32) -> String {
+    let seconds = ms / 1000;
+    let minutes = (seconds / 60) % 60;
+    let hours = (minutes / 60) % 60;
+
+    format!(
+        "{:0>2}:{:0>2}:{:0>2}.{:0>3}",
+        hours,
+        minutes,
+        seconds,
+        ms % 1000
+    )
 }
 
 fn main() -> anyhow::Result<()> {
     let model = load_model().expect("failed to load model");
     println!("{}", model.metadata()?.description()?);
+
     let mut wav: Wav<i16> = Wav::from_path("./en.wav").expect("failed to load audio file");
     let samples: Samples<i16> = wav.read()?;
     let (sample_rate, channels, duration, encoding) = wav.wav_spec();
@@ -42,19 +55,18 @@ fn main() -> anyhow::Result<()> {
 
     let mut speech_started = false;
 
-    let mut time = 0;
+    let mut elapsed_time_ms = 0;
 
-    for chunk in data.chunks(get_window_size()) {
+    for chunk in data.chunks(get_window_size(sample_rate)) {
         let chunk_array = ndarray::Array1::from_iter(chunk.to_owned());
 
+        // making it a 2D array?
         let input_array = chunk_array.view().insert_axis(Axis(0));
 
         let input = ort::Value::from_array(input_array).expect("failed to make input array");
         let sr = ort::Value::from_array(ndarray::array![sample_rate as i64])?;
         let h = ort::Value::from_array(Array3::<f32>::zeros((2, 1, 64)))?;
         let c = ort::Value::from_array(Array3::<f32>::zeros((2, 1, 64)))?;
-        // println!("input {:?}", chunk);
-        // we need an sr and h and c in the same format as input i.e. the array thing
 
         let input_data = inputs![
             "input" => input,
@@ -68,18 +80,26 @@ fn main() -> anyhow::Result<()> {
         let tensor = outputs["output"].extract_tensor::<f32>()?;
 
         for value in tensor.view().iter() {
-            if value.to_owned() > 0.5 && speech_started == false {
-                println!("{}ms detected speech start ({})", time, value);
+            if value.to_owned() > 0.5 && !speech_started {
+                println!(
+                    "{} detected speech start ({})",
+                    format_time(elapsed_time_ms),
+                    value
+                );
 
                 speech_started = true;
             } else if value.to_owned() < 0.35 && speech_started {
-                println!("{}ms detected speech end ({})", time, value);
+                println!(
+                    "{} detected speech end ({})",
+                    format_time(elapsed_time_ms),
+                    value
+                );
 
                 speech_started = false;
             }
         }
 
-        time += 100;
+        elapsed_time_ms += 100;
     }
 
     Ok(())
